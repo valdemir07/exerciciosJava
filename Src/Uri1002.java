88286b69921277afdb0e78c4afd157263142bbe5
Valdemir Silva <49254007+valdemir07@users.noreply.github.com>
2022-09-02 13:20:50 -0300

    Update Uri1002.java

--- Src/Uri1002.java
+++ Src/Uri1002.java
@@ -14,18 +14,17 @@ public class AreaCirculo {
 
 	public static void main(String[] args) {
 		
-		Locale.setDefault(Locale.US);
-		Scanner sc = new Scanner (System.in); 
+	Locale.setDefault(Locale.US);
+	Scanner sc = new Scanner (System.in); 
 		
-		double raio, area, n;
+double raio, area, n;
 		
-		raio = sc.nextFloat();
-		n = 3.14159;
-		
-		area = n * (raio * raio);
+	raio = sc.nextFloat();
+	n = 3.14159;
+      
+	area = n * (Math.pow(raio,2)); //area = n * (raio * raio);
 
-	System.out.printf ("A = %.4f\n", area);
-	sc.close();
-
+System.out.printf ("A = %.4f\n", area);
+sc.close();
 	}
 }
