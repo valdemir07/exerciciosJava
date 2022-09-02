60c0be992a357f5fe5db79035c975100aac727a5
Valdemir Silva <49254007+valdemir07@users.noreply.github.com>
2022-09-01 22:06:05 -0300

    Update Uri1004.java

--- src/Uri1004.java
+++ src/Uri1004.java
@@ -1,23 +1,22 @@
 /**
 Exercicio 1004 beecrowd | Produto Simples | https://www.beecrowd.com.br/judge/pt/problems/view/1004
 */
-
 import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
+ public static void main(String[] args) {
+ Scanner sc = new Scanner(System.in);
 		
-		  int A, B, PROD;
+   int A, B, PROD;
 		
-		  A = sc.nextInt();
-		  B = sc.nextInt();
+  	A = sc.nextInt();
+	B = sc.nextInt();
 		
-		PROD = A * B;
+   PROD = A * B;
 		
 System.out.println("PROD = "+ PROD);
 
 	  sc.close();
-	    }
+   }
 }
