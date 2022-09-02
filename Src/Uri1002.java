02e323030703acf180ff3ba1294c1d0073d5d279
Valdemir Silva <49254007+valdemir07@users.noreply.github.com>
2022-09-02 13:04:33 -0300

    Create Uri1002.java

---
+++ Src/Uri1002.java
@@ -0,0 +1,31 @@
+/**
+Exercicio - Calculo Área do Círculo
+area = n.raio²
+
+n = 3.14159 | Imprimir resultado com 4 casas decimais após o ponto decimal
+*/
+
+package exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AreaCirculo {
+
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in); 
+		
+		double raio, area, n;
+		
+		raio = sc.nextFloat();
+		n = 3.14159;
+		
+		area = n * (raio * raio);
+
+	System.out.printf ("A = %.4f\n", area);
+	sc.close();
+
+	}
+}
